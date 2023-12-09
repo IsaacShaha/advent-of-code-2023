21aaaaa28619a00a86e855f8090bc7b6527256aa
Isaac Shaha <isaac.shaha64@gmail.com>
2023-12-09 12:26:55 -0800

    Finished mirage maintenance 1 & 2

---
+++ app/MirageMaintenance2.hs
@@ -0,0 +1,43 @@
+module MirageMaintenance2 where
+
+import           Data.Sequence      (Seq (Empty, (:<|), (:|>)), fromList,
+                                     reverse)
+import           Prelude            hiding (reverse)
+import           Text.Parsec        (char, digit, many1, newline, option,
+                                     sepBy1)
+import           Text.Parsec.String (Parser)
+import           Utils              (unsafeParseFromFile)
+
+-- Input Parsing
+
+fileParser :: Parser (Seq (Seq Int))
+fileParser = fromList <$> sepBy1 valueHistoryParser newline
+
+intParser :: Parser Int
+intParser = fmap read $ (:) <$> option ' ' (char '-') <*> (many1 digit)
+
+valueHistoryParser :: Parser (Seq Int)
+valueHistoryParser = fromList <$> sepBy1 intParser (char ' ')
+
+-- Logic
+
+diffs :: Seq Int -> Seq Int
+diffs Empty              = Empty
+diffs (n :<| Empty)      = Empty
+diffs (n1 :<| n2 :<| ns) = n2 - n1 :<| diffs (n2 :<| ns)
+
+next :: Seq Int -> Int
+next ns
+  | all (== 0) ns = 0
+  | otherwise = seqLast ns + (next . diffs $ ns)
+
+prev :: Seq Int -> Int
+prev = next . reverse
+
+seqLast :: Seq a -> a
+seqLast (_ :|> a) = a
+
+main :: IO ()
+main
+    = unsafeParseFromFile fileParser "mirage-maintenance.txt"
+  >>= print . sum . fmap prev
