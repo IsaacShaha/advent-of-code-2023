3a9320a46bcb29f3d255f26c1ae3f4c5dcda6547
Isaac Shaha <isaac.shaha64@gmail.com>
2023-12-15 08:20:49 -0800

    Finished day 15 part 2

--- app/LensLibrary2.hs
+++ app/LensLibrary2.hs
@@ -1,12 +1,9 @@
-module LensLibrary1 where
+module LensLibrary2 where
 
-import qualified Data.Char     as DC
-import qualified Data.Foldable as DF
-import qualified Data.List     as DL
-import qualified Text.Parsec   as TP
-import qualified Utils         as U
-
-import           Debug.Trace
+import qualified Data.Char   as DC
+import qualified Data.List   as DL
+import qualified Text.Parsec as TP
+import qualified Utils       as U
 
 -- | Parsing
 
@@ -20,35 +17,56 @@ stepParser = TP.many1 $ TP.noneOf ","
 
 -- | Logic
 
-type FocalLength = Int
+replace :: Int -> a -> [a] -> [a]
+replace index element list
+  = take index list ++ [element] ++ drop (index + 1) list
+
 type Label = String
-data Lens = Lens Label FocalLength deriving (Eq, Show)
+
+hash :: Label -> Int
+hash = DL.foldl' (\hash -> (`mod`256) . (*17) . (+hash) . fromEnum) 0
+
+type Box = [Lens]
+
+type FocalLength = Int
+
+data Lens = Lens {
+    label       :: Label
+  , focalLength :: FocalLength
+  } deriving (Eq, Show)
+
+boxesPower :: [Box] -> Int
+boxesPower = sum . zipWith (*) [1..] . fmap boxPower
+
+boxPower :: Box -> Int
+boxPower = sum . zipWith (*) [1..] . fmap focalLength
+
+insertLens :: Lens -> Box -> Box
+insertLens newLens@(Lens lensLabel _) box
+  = case DL.findIndex ((== lensLabel) . label) box of
+      Just index -> replace index newLens box
+      Nothing    -> box ++ [newLens]
+
+removeLabel :: Label -> Box -> Box
+removeLabel = filter . (. label) . (/=)
+
 data Operation = Insert FocalLength | Remove deriving (Eq, Show)
+
 type Step = String
 
-applyBox :: Step -> [Lens] -> [Lens]
+applyBox :: Step -> Box -> Box
 applyBox step box = case operation step of
-  Insert focalLength -> insert (Lens (label step) focalLength) box
-  Remove             -> undefined
+  Insert focalLength -> insertLens (Lens (labelFromStep step) focalLength) box
+  Remove             -> removeLabel (labelFromStep step) box
 
-applyBoxes :: Step -> [[Lens]] -> [[Lens]]
+applyBoxes :: Step -> [Box] -> [Box]
 applyBoxes step boxes
-  = take boxIndex boxes
- ++ applyBox step (boxes !! boxIndex)
-  : drop (boxIndex + 1) boxes
+  = replace boxIndex (applyBox step (boxes !! boxIndex)) boxes
   where
-    boxIndex = hash step
+    boxIndex = hash . labelFromStep $ step
 
-hash :: Step -> Int
-hash = DF.foldl' (\hash -> (`mod`256) . (*17) . (+hash) . fromEnum) 0
-
-insert :: Lens -> [Lens] -> [Lens]
-insert lens lenses = case DL.elemIndex lens lenses of
-  Just index -> take index lenses ++ [lens] ++ drop (index + 1) lenses
-  Nothing    -> lenses ++ [lens]
-
-label :: Step -> Label
-label = takeWhile DC.isAlpha
+labelFromStep :: Step -> Label
+labelFromStep = takeWhile DC.isAlpha
 
 operation :: Step -> Operation
 operation step = case dropWhile DC.isAlpha $ step of
@@ -57,7 +75,8 @@ operation step = case dropWhile DC.isAlpha $ step of
   other             -> error $ "Invalid step " ++ show other ++ "."
 
 main :: IO ()
-main = do
-  steps <- U.unsafeParseFromFile fileParser "lens-library.txt"
-  let boxes = replicate 256 [] :: [[Lens]]
-  print $ applyBoxes (steps !! 0) boxes
+main
+    = print
+    . boxesPower
+    . DL.foldl' (flip applyBoxes) (replicate 256 [])
+  =<< U.unsafeParseFromFile fileParser "lens-library.txt"
